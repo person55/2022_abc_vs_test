dbf5d526452a58f2449aa4e1e9f4dd6be3dee439
person55 <kyh.sohn@gmail.com>
2022-06-03 12:59:42 +0900

    “레벨디자인”

---
+++ Assets/CubeCtrl.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeCtrl : MonoBehaviour
+{
+    public float moveSpeed = 10.0f;
+    public float turnSpeed = 80.0f;
+    
+    private float h;
+    private float v;
+
+    private void Update()
+    {
+        h = Input.GetAxis("Horizontal");  // -1.0f ~ 1.0f
+        v = Input.GetAxis("Vertical");     // -1.0f ~ 1.0f
+
+        Move();
+    }
+
+    private void Move()
+    {
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime * v);
+        transform.Rotate(Vector3.up * turnSpeed * Time.deltaTime * h);
+    }
+}
+
